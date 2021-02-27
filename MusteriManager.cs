f9dbc16d8083bec3556908f4f665920fd76a5a74
enisarisin <enisarisin@gmail.com>
2021-02-27 21:37:19 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void GetMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Name + " " + musteri.Surname);
+        }
+
+        public void AddMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + "Eklendi.");
+        }
+
+        public void DeleteMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + "Silindi.");
+        }
+
+        public bool Validation(Musteri musteri)
+        {
+            if (musteri.Id / 100 <= 9 && musteri.Id / 100 >= 1) //Validation number must be 3 digits.
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 123;
+            musteri1.Name = "Jack";
+            musteri1.Surname = "Ma";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 456;
+            musteri2.Name = "Jeff";
+            musteri2.Surname = "Bezos";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 789;
+            musteri3.Name = "Elon";
+            musteri3.Surname = "Musk";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri[] musteriArray = new Musteri[]{musteri1, musteri2, musteri3};
+
+            foreach (Musteri musteri in musteriArray)
+            {
+                musteriManager.AddMusteri(musteri);
+            }
+
+            Console.WriteLine("******************************************************");
+
+            foreach (Musteri musteri in musteriArray)
+            {
+                if (musteriManager.Validation(musteri) == true)
+                {
+                    musteriManager.GetMusteri(musteri);
+                }
+                else
+                {
+                    Console.WriteLine("Gecersiz Id");
+                }
+            }
+
+            foreach (Musteri musteri in musteriArray)
+            {
+                musteriManager.DeleteMusteri(musteri);
+            }
+            
+        }
+    }
+}
